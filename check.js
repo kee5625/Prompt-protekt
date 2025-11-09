36ff17e92d4cf9799607cae82b0f4294cd64aa55
Aditya Kulkarni <97181282+AdityaKulkarni2706@users.noreply.github.com>
2025-11-08 21:45:47 -0500

    Fix input string formatting in predict call

--- check.js
+++ check.js
@@ -5,7 +5,7 @@ const client = await Client.connect("Goofybaka/pii");
 
 // 2. Call predict with an ARRAY of inputs
 const result = await client.predict("/predict", [ 
-    "Hello!! MY ssn is 123-20-4329 and i work at apple.co as director"  // <-- This is the fix
+    "Hello!! MY ssn is 123-20-4329 and i work at apple.co as director"  
 ]);
 
 console.log(result.data);
